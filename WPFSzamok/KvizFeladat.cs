71383c7705fefa4d8d69e10f4b49723e96988343
tubokopajl <kopajler@gmail.com>
2024-01-31 18:49:59 +0100

    Add project files.

---
+++ WPFSzamok/KvizFeladat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSzamok
+{
+    internal class KvizFeladat
+    {
+        public string Kerdes { get; private set; }
+
+        public int Valasz  { get; private set; }
+
+        public int Pont { get; private set; }
+
+        public string Kategoria { get; private set; }
+
+
+        public KvizFeladat(string sor1, string sor2)
+        {
+            //sor1=Kerdes
+            Kerdes = sor1;
+            //sor2=Valasz and Pont and Kategoria
+            string[] adatok = sor2.Split(' ');
+            Valasz = int.Parse(adatok[0]);
+            Pont = int.Parse(adatok[1]);
+            Kategoria = adatok[2];
+
+        
+        }
+    }
+}
---
+++ WPFSzamok/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WPFSzamok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<KvizFeladat> feladatok;
+        public MainWindow()
+        {
+            feladatok = new();
+            InitializeComponent();
+            FajlFeltolt();
+            Fel2();
+            Fel3();
+            Fel4();
+            Fel5();
+        }
+
+        void FajlFeltolt()
+        {
+            using (StreamReader sr = new StreamReader("felszam.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string sor1 = sr.ReadLine();
+                    string sor2 = sr.ReadLine();
+
+                    if (sor2 != null)
+                    {
+                        KvizFeladat feladat = new(sor1, sor2);
+                        feladatok.Add(feladat);
+                    }
+                }
+            }
+
+
+
+
+
+        }
+
+        void Fel2()
+        {
+            //feladatok száma
+            tbFel2.Text = feladatok.Count.ToString();
+
+        }
+
+        void Fel3()
+        {
+            var matek = feladatok.Where(x => x.Kategoria == "matematika").ToList();
+            string count = matek.Count.ToString();
+            string egyPont = matek.Where(x => x.Pont == 1).Count().ToString();
+            string kettoPont = matek.Where(x => x.Pont == 2).Count().ToString();
+            string haromPont = matek.Where(x => x.Pont == 3).Count().ToString();
+
+            string labelText =string.Format( $" Az adatfájlban {count} matematika feladat van\n1 pontot ér {egyPont} feladat\n2 pontot ér {kettoPont} feladat\n3 pontot ér {haromPont} feladat");
+            lbFel3.Text = labelText;
+            
+        }
+
+        void Fel4()
+        {
+            var valaszokMin= feladatok.Min(x => x.Valasz);
+            var valaszokMax = feladatok.Max(x => x.Valasz);
+            tbFel4a.Text=valaszokMin.ToString();
+            tbFel4b.Text = valaszokMax.ToString();
+            
+        }
+
+        void Fel5()
+        {
+            var temaKorok = feladatok.Select(x => x.Kategoria).Distinct().ToList();
+            cbFel5.ItemsSource = temaKorok;
+        }
+
+
+    }
+}
